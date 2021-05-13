0ef7966cb54b20a88b371060e19155954c6616be
Valraox <evilman_morgoth@hotmail.com>
2021-05-13 12:44:01 +0200

    Añadidos métodos para obtener medidas de CO2

--- controllers/co2-measure.js
+++ controllers/co2-measure.js
@@ -4,6 +4,11 @@ var validator = require('validator');
 var CO2Measure = require('../models/co2-measure');
 
 var controller = {
+    /* 
+     ** Método para almacenar en base de datos
+     ** Parámetros POST: deviceId: String, CO2ppm: Number
+     ** return HTTP response
+     */
     save: (req, res) => {
         // Recoger parámetros por post
         var params = req.body;
@@ -43,7 +48,83 @@ var controller = {
                 });
             });
         }
-    }
+    },
+
+    /*
+     ** Método para devolver todas las unidades de medida
+     ** GET
+     ** URL params: sort?
+     ** return HTTP response
+     */
+    getAllMeasures: (req, res) => {
+        // Recoger variable de campo a ordenar de la URL
+        var sort = req.params.sort;
+
+        // Find
+        CO2Measure.find({}).sort(sort).exec((err, measures) => {
+
+            // Comprobar errores
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al obtener las medidas'
+                });
+            }
+
+            // Comprobar datos devueltos
+            if (!measures) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay medidas de CO2'
+                });
+            } else {
+                // Devolver respuesta con datos
+                return res.status(200).send({
+                    status: 'success',
+                    measures
+                });
+            }
+        });
+    },
+
+    /*
+     ** Método para devolver todas las unidades de medida
+     ** GET
+     ** URL params: deviceId
+     ** return HTTP response
+     */
+    getDeviceMeasures: (req, res) => {
+        // Recoger id de dispositivo
+        var deviceId = req.params.deviceId;
+
+        // Buscar el artículo
+        CO2Measure.find({
+            deviceId: deviceId
+        }).exec((err, measures) => {
+
+            // Comprobar errores
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al obtener las medidas del dispositivo ' + deviceId
+                });
+            }
+
+            // Comprobar datos devueltos
+            if (!measures) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay medidas de CO2 para el dispositivo ' + deviceId
+                });
+            } else {
+                // Devolver una respuesta con datos
+                return res.status(200).send({
+                    status: 'success',
+                    measures
+                });
+            }
+        });
+    },
 };
 
 module.exports = controller;
--- routes/co2-measure-routes.js
+++ routes/co2-measure-routes.js
@@ -7,5 +7,7 @@ var router = express.Router();
 
 // Rutas para medidas de CO2
 router.post('/save', CO2MeasureController.save);
+router.get('/get-all/:sort?', CO2MeasureController.getAllMeasures);
+router.get('/get-measures/:deviceId', CO2MeasureController.getDeviceMeasures);
 
 module.exports = router;
